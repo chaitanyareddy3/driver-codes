0bd4ffcaac228c1517b1883d5cfd8d79fc89f042
chaitanyareddy3 <103244056+chaitanyareddy3@users.noreply.github.com>
2022-04-08 00:30:35 -0700

    Update python-driver-code.py

--- numbers/python-driver-code.py
+++ numbers/python-driver-code.py
@@ -1,10 +1,16 @@
 # Python Driver Code
 
 def solve(n: int) -> str:
+  while (n%2==0):
+n=n//2
+while (n%7==0):
+n=n//7
+if (n==1):
   # Your code goes here
   # n is the given input
   return "Special"
-
+else
+return "regular"
 # The following snippet reads the input in the required format. 
 # Just complete the solve function above. 
 
